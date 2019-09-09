d89ac36a88d833cccddbb3dfea133831d817ff37
Oleg Zarevych <Oleg.Zarevych@gmail.com>
2019-09-10 01:46:25 +0300

    add POJOs

---
+++ src/main/java/POJO/ListUsers.java
@@ -0,0 +1,16 @@
+package POJO;
+
+import java.util.List;
+
+public class ListUsers {
+
+    public int page;
+
+    public int per_page;
+
+    public int total;
+
+    public int total_pages;
+
+    public List<User> data;
+}
---
+++ src/main/java/POJO/User.java
@@ -0,0 +1,14 @@
+package POJO;
+
+public class User {
+
+    public int Id;
+
+    public String email;
+
+    public String first_name;
+
+    public String last_name;
+
+    public String avatar;
+}
